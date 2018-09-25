f23eaaaf1f9850d9ad4052d033a53ef62a02bccd
Brooks Patton <brooks@brookzerker.com>
2018-09-25 08:03:35 -0600

    WIP - storing the command into an action

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Rust todo plan
 
-* [ ] Display list of todo items
 * [ ] Add item to list
+* [x] Display list of todo items
 * [ ] Mark item as completed
 * [ ] Undo the mark item as completed
 * [ ] Delete the item from the list
-* [ ] Change the item
 
 ## Stretch goals
 
+* [ ] Change the item
 * [ ] Customize the order
+
+---
+
+There is a possibility of going multiple directions with handling each action. Next will be checking the rust book for how they handled arguments with data.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use std::env;
+
+struct TodoItem {
+    task: String,
+    completed: char,
+}
+
+impl TodoItem {
+    fn new(task: String) -> TodoItem {
+        TodoItem {
+            task: task,
+            completed: ' '
+        }
+    }
+}
+
+struct TodoList {
+    list: Vec<TodoItem>
+}
+
+impl TodoList {
+    fn new() -> TodoList {
+        let list = Vec::new();
+
+        TodoList {
+            list: list
+        }
+    }
+
+    fn add_task(&mut self, task: String) {
+        let todo_item = TodoItem::new(task);
+
+        self.list.push(todo_item);
+    }
+
+    fn display_tasks(&self) {
+        let mut count = 0;
+
+        for todo_item in &self.list {
+            println!("{} - [{}] {}", count, todo_item.completed, todo_item.task);
+            count += 1;
+        }
+    }
+}
+
+enum Actions {
+    Add,
+    Done,
+    Undone,
+    Delete
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env::args().collect();
+    // println!("{:?}", arguments);
+    let command;
+
+    if arguments[1] == "add" {
+        let task = String::from(arguments[2]);
+        command = Arguments::Add(arguments[2]);
+    }
+
+    let mut list = TodoList::new();
+    let first_task = String::from("Mop floor");
+    let second_task = String::from("Grocery shopping");
+
+    list.add_task(first_task);
+    list.add_task(second_task);
+    list.display_tasks();
 }
